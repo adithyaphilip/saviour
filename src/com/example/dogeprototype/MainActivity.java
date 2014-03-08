c41c0261723301fa4164f730cb647876b0da70b3
Adithya <adithyaphilip@bitbucket.org>
2014-03-09 03:30:52 +0530

    lol

--- src/com/example/dogeprototype/MainActivity.java
+++ src/com/example/dogeprototype/MainActivity.java
@@ -1,10 +1,5 @@
 package com.example.dogeprototype;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
 public class MainActivity extends Activity {
 
 	@Override
